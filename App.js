4c3a0565f45f6fa95f7bdb7a2f95c970bdcdb952
eboriley <eboriley@hotmail.co.uk>
2021-04-16 05:01:21 +0000

    third update

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Students from './components/Students'
+import ViewStudent from './components/ViewStudent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>abmock</h3>
+      <Router>
+      <Switch>
+      <Route exact path="/students" component={Students}/>
+      <Route exact path="/viewstudent/:id" component={ViewStudent}/>
+      </Switch>
+      </Router>
+      <script src="/__/firebase/8.4.1/firebase-app.js"></script>
+    
+    
+    <script src="/__/firebase/8.4.1/firebase-analytics.js"></script>
+    
+    
+    <script src="/__/firebase/init.js"></script>
     </div>
   );
 }
