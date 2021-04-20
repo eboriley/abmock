047d0f0d1a67bfc2940ba7eeefd096e20867c10d
eboriley <eboriley@hotmail.co.uk>
2021-04-20 18:33:43 +0000

    Added some component

--- App.js
+++ App.js
@@ -1,26 +1,38 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {useSelector} from 'react-redux'
 import Students from './components/Students'
 import ViewStudent from './components/ViewStudent';
+import NavBar from './components/NavBar';
+import UpdateStudent from './components/UpdateStudent';
+import Login from './components/Login';
+import LogggedInTeacher from './components/LogggedInTeacher';
+import LandingPage from './components/LandingPage';
 
 function App() {
+  const user = useSelector(state =>state.user)
+  const teacherLogin = useSelector(state =>state.teacherLogin)
   return (
     <div className="App">
-      <h3>abmock</h3>
+      {!user ? <></>: <LogggedInTeacher/>}
       <Router>
-      <Switch>
-      <Route exact path="/students" component={Students}/>
-      <Route exact path="/viewstudent/:id" component={ViewStudent}/>
-      </Switch>
+        {
+          !user ? (<Login/>):(
+            <>
+            <NavBar/>
+            <Switch>
+            <Route exact path="/" component={LandingPage}/>  
+            <Route exact path="/students" component={Students}/>
+            <Route exact path="/viewstudent/:id" component={ViewStudent}/>
+            {!teacherLogin ? (<p>loading ...... </p>): ( <Route exact path="/updatestudent/:id" component={UpdateStudent}/>)}
+            </Switch>
+            </>
+          )
+        }
+     
       </Router>
-      <script src="/__/firebase/8.4.1/firebase-app.js"></script>
-    
-    
-    <script src="/__/firebase/8.4.1/firebase-analytics.js"></script>
-    
-    
-    <script src="/__/firebase/init.js"></script>
     </div>
   );
 }
